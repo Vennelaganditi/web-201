7adcbc039ce562e3fdc926173a11c3a5f61e0c1a
Vennelaganditi <113632491+Vennelaganditi@users.noreply.github.com>
2022-11-14 22:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,54 @@
-const http = require('http')
-const fs= require("fs");
+const http = require("http");
+const fs = require("fs");
+const args = require('minimist')(process.argv.slice(2));
 
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 
-
-
-
-
-let homecontent="";
-let filecontent="";
-let reg="";
-fs.readFile("home.html",
-(err,home) => {
-if(err){
+fs.readFile("home.html", (err, home) => {
+  if (err) {
     throw err;
-}
-homecontent=home;
+  }
+  homeContent = home;
 });
 
-fs.readFile("project.html",
-(err,fil) => {
-    if(err)
-    {
-        throw err;
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
     }
-   filecontent=fil;
-})
-
-
-
-
-
-
-
-
-fs.readFile("registration.html",
-(err,regf) => {
-    if(err)
-    {
-        throw err;
-    }
- reg=regf;
-})
-let args=require("minimist")(process.argv.slice(2));
-http.createServer((request,response) =>{
-    let url =request.url
-    response.writeHeader(200,{"Content-type":"text/html"})
-    switch (url)
-    {
-        case "/project":
-            response.write(filecontent)
-            response.end();
-            break
-        case "/registration":
-            response.write(reg)
-            response.end();
-            break;
-        default:
-            response.write(homecontent)
-            response.end();
-            break;
-    }
-}).listen(args["port"]);
+  })
+  .listen(args.port);
+});
